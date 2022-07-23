e76d74ed750d564187e68c2f185f826c23267c5a
Shinyepo <69401477+Shinyepo@users.noreply.github.com>
2022-07-24 01:07:39 +0200

    Update mikro-config.ts

--- mikro-config.ts
+++ mikro-config.ts
@@ -1,7 +1,12 @@
 import { Options } from "@mikro-orm/core";
 import { PostgreSqlDriver } from "@mikro-orm/postgresql";
+import path from "path";
 
 export default {
+  migrations: {
+    path: path.join(__dirname, "./migrations"),
+    pattern: /^[\w-]+\d+\.[tj]s$/,
+  },
   entities: [],
   type: "",
   host: "",
